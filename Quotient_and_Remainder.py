8d2b9517682cdb4bd9a12fe720fbbad01af19cda
Samidha Samal <72935081+Somu30@users.noreply.github.com>
2021-10-05 22:33:13 +0530

    Create Quotient_and_Remainder.py

---
+++ Quotient_and_Remainder.py
@@ -0,0 +1,12 @@
+def find(n, m):
+      
+    # for quotient
+    q = n//m
+    print("Quotient: ", q)
+      
+    # for remainder
+    r = n%m
+    print("Remainder", r)
+ 
+find(10, 3)
+find(99, 5)
